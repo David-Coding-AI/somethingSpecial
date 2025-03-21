89abd975f8354666d42f54522cc607bae67330b6
David-Coding-AI <dyurchuk@seagullscientific.com>
2025-03-20 17:10:39 -0700

    Initial Commit

---
+++ step_definitions/steps.ts
@@ -0,0 +1,12 @@
+import {When} from "cucumber";
+
+const { I } = inject();
+// Add in your custom step files
+
+Given('I have a defined step', () => {
+  // TODO: replace with your own step
+});
+
+When(/^I do something$/, function () {
+
+});
