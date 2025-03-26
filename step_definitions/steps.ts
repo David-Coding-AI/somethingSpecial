ce6b03e95dc752710c243df62287a1093e33e1be
David-Coding-AI <dyurchuk@seagullscientific.com>
2025-03-25 18:30:17 -0700

    Initial Commit

--- step_definitions/steps.ts
+++ step_definitions/steps.ts
@@ -17,12 +17,16 @@ Given(/^I am in BTC$/, function () {
 });
 When(/^I login with valid credentials$/, function () {
     I.amOnPage('/');
-    I.wait(5);
-    I.fillField('input[id="1-email"]', 'Thisisme@gmail.com');
-    I.fillField('input[id="1-password"]', 'AndYou');
-    I.click('//*[@id="1-submit"]')
-    I.wait(5);
+    I.wait(2);
+    I.fillField('input[id="1-email"]', 'dyurchuk+prodam1@seagullscientific.com');
+    I.fillField('input[id="1-password"]', 'Am1DevCluster@');
+    I.click('//*[@id="1-submit"]');
+    I.wait(2);
+    I.click('//*[@id="profileDropdown"]');
+    I.click('//*[@id="root"]/div[1]/div[2]/div/div[2]/div/div/div[2]/a');
+    I.wait(2);
     // id="1-email";
     // id="1-password";
     // id="1-submit";
+    // id="profileDropdown";
 });
