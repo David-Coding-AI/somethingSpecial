f4814a9897b46698743cb3d974c5782c7101e314
David-Coding-AI <dyurchuk@seagullscientific.com>
2025-04-01 19:10:44 -0700

    Initial Commit

---
+++ pages/installer.pages.ts
@@ -0,0 +1,19 @@
+
+class PrinterInstaller {
+    private locators = {
+        nextButton: "//Button[@Name='Next >']",
+        finishButton: "//Button[@Name='Finish']",
+    };
+
+    get nextButton() {
+        return this.locators.nextButton;
+    }
+
+    get finishButton() {
+        return this.locators.finishButton;
+    }
+}
+
+export default new PrinterInstaller();
+
+
--- step_definitions/steps_desktop.ts
+++ step_definitions/steps_desktop.ts
@@ -1,11 +1,12 @@
 import path from "path";
 import * as fs from "fs";
 import {exec} from "child_process";
+import installerPages from "../pages/installer.pages";
 
 const {I} = inject();
 
 Given(/^The user has installed a driver on the system$/, function () {
-    const exeName = "Zebra_2024.3.exe";
+    const exeName = "Zebra.exe";
     const downloadsPath = path.join(process.env.USERPROFILE || "", "Downloads", exeName);
 
     if (!fs.existsSync(downloadsPath)) {
@@ -22,5 +23,12 @@ Given(/^The user has installed a driver on the system$/, function () {
         console.log(stdout);
     });
     I.wait(3);
-    I.click("//RadioButton[@Name='I accept the terms in the license agreement']");
+    I.pressKey(["Alt","a"]);
+    I.click(installer.nextButton)
+    I.click(installer.nextButton)
+    I.pressKey(["Alt","r"]);
+    I.click(installer.finishButton)
+
+    //I.click("//RadioButton[@Name='I accept the terms in the license agreement']");
+    //I.click("  //Button[@Name='\\"Next >\\"' and @AutomationId='\"12324\"' and @ControlType='Button']\n")
 });
